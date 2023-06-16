18cdc02769311b7c62c6a4adcd8447fbf03502e4
DanierTrochez <yenifernataly2003@gmail.com>
2023-06-15 21:27:28 -0500

    primer commit

---
+++ proyectos/views/agregar_integrantes.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from proyectos.serializers.lista_inscritos import ListaInscritoSerializer
+from proyectos.models import Inscrito
+from proyectos.views.funciones import perfil_conectado
+
+class AgregarIntegrantesViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Inscrito.objects.all()
+    serializer_class = ListaInscritoSerializer
+
+    @action(detail=False, methods=['get'], url_path='inscritos/(?P<id_user>\d+)')
+    def get_inscritos(self, request, id_user=None):
+        # Obtener el perfil_id del usuario logueado
+        perfil_id = perfil_conectado(id_user)
+
+        # Obtener los inscritos que no son el usuario logueado y cumplen las condiciones
+        inscritos = Inscrito.objects.exclude(perfil_id=perfil_id).filter(estado='activo', nombre_grupo__isnull=True)
+
+        # Serializar los datos de los inscritos
+        serializer = self.get_serializer(inscritos, many=True)
+
+        # Devolver la respuesta con los datos serializados
+        return Response(serializer.data)
